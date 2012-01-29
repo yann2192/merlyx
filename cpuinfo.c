b21f8e4f3ef978b7b03680c8267625f3125db76d
Yann GUIBET <yannguibet@gmail.com>
2012-01-29 21:02:27 +0100

    add cpuinfo

---
+++ cpuinfo.c
@@ -0,0 +1,107 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+struct eax0
+{
+    unsigned int lsfns; /* Largest Standard Function Number Supported */
+    char vendor[13];
+};
+
+typedef struct eax0 EAX0;
+
+struct eax1
+{
+    unsigned short stepping;
+    unsigned short model;
+    unsigned short family;
+    unsigned short cputype;
+    unsigned int extendedmodel;
+    unsigned int extendedfamily;
+};
+
+typedef struct eax1 EAX1;
+
+struct cpuid_info
+{
+    EAX0 info1;
+    EAX1 info2;
+};
+
+typedef struct cpuid_info * CPUID_INFO;
+
+static void getVendor(EAX0 * res)
+{
+    int * t = (int *)res->vendor;
+    asm("xor %%rax, %%rax\n"
+        "cpuid\n"
+        "mov %%eax, %0\n"
+        "mov %%ebx, %1\n"
+        "mov %%edx, %2\n"
+        "mov %%ecx, %3\n"
+        : "=r" (res->lsfns), "=r" (t[0]), "=r" (t[1]), "=r" (t[2]) /* OUTPUT */
+        : /* INPUT */
+        : "%eax", "%ebx", "%ecx", "edx" /* clobbered register */
+       );
+    res->vendor[12] = '\0';
+}
+
+static void getProcessorInfo(EAX1 * res)
+{
+    int e[4];
+    asm("mov $1, %%rax\n"
+        "cpuid\n"
+        "mov %%eax, %0\n"
+        "mov %%ebx, %1\n"
+        "mov %%ecx, %2\n"
+        "mov %%edx, %3\n"
+        : "=r" (e[0]), "=r" (e[1]), "=r" (e[2]), "=r" (e[3]) /* OUTPUT */
+        : /* INPUT */
+        : "%eax", "%ebx", "%ecx", "edx" /* clobbered register */
+       );
+    res->stepping = e[0] & 0xF; /* 0-3 */
+    res->model = e[0] & 0xF0; /* 4-7 */
+    res->family = e[0] & 0xF00; /* 8-11 */
+    res->cputype = e[0] & 0x3000; /* 12-13 */
+    res->extendedmodel = e[0] & 0xF0000; /* 16-19 */
+    res->extendedfamily = e[0] & 0xFF00000; /* 20-27 */
+}
+
+CPUID_INFO CPUID_INFO_create()
+{
+    CPUID_INFO res = malloc(sizeof(struct cpuid_info));
+    getVendor(&(res->info1));
+    getProcessorInfo(&(res->info2));
+    return res;
+}
+
+void CPUID_INFO_free(CPUID_INFO p)
+{
+    free(p);
+}
+
+void CPUID_INFO_fprintf(FILE * f, CPUID_INFO info)
+{
+    fprintf(f, "Largest Standard Function Number Supported : %i\n", info->info1.lsfns);
+    fprintf(f, "Vendor : %s\n\n", info->info1.vendor);
+    fprintf(f, "Stepping : %i\n", info->info2.stepping);
+    fprintf(f, "Model : %i\n", info->info2.model);
+    fprintf(f, "Family : %i\n", info->info2.family);
+    fprintf(f, "CPU type : ");
+    switch(info->info2.cputype){
+        case 0: fprintf(f, "Original\n"); break;
+        case 1: fprintf(f, "OverDrive\n"); break;
+        case 2: fprintf(f, "Dual\n"); break;
+        default: fprintf(f, "Unknown (%i)\n", info->info2.cputype);
+    }
+    fprintf(f, "Extended model : %i\n", info->info2.extendedmodel);
+    fprintf(f, "Extended family : %i\n", info->info2.extendedfamily);
+}
+
+int main(int argc, const char *argv[])
+{
+    CPUID_INFO info = CPUID_INFO_create();
+    CPUID_INFO_fprintf(stdout, info);
+    CPUID_INFO_free(info);
+    return 0;
+}
