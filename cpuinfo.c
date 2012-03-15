73dd6066035774e296b3cec9aa677b085884b67d
Yann GUIBET <yannguibet@gmail.com>
2012-03-15 21:59:43 +0100

    correction d'un bug pour les caches intel

--- cpuinfo.c
+++ cpuinfo.c
@@ -133,8 +133,15 @@ void getCachesInfo(EAX2 * res)
 
     nb = e[0] & 0xFF;
     printf("nb => 0x%x\n", nb);
-    for(i=1; i < 15; ++i) {
-        tmp = (e[0] >> 0x8*i) & 0xFF;
+    for(i=1; i < 16; ++i) {
+        if(i < 4)
+            tmp = (e[0] >> 0x8*i) & 0xFF;
+        else if(i < 8)
+            tmp = (e[1] >> 0x8*(i%4)) & 0xFF;
+        else if(i < 12)
+            tmp = (e[2] >> 0x8*(i%4)) & 0xFF;
+        else
+            tmp = (e[3] >> 0x8*(i%4)) & 0xFF;
         switch(tmp){
             case 0x01:
                 res->caches[i-1] = "Instruction TLB: 4-KB Pages, 4-way set associative, 32 entries";
@@ -444,76 +451,76 @@ void CPUID_INFO_fprintf(FILE * f, CPUID_INFO info)
 {
     int i;
     fprintf(f, "Largest Standard Function Number Supported : 0x%x\n", info->info1.lsfns);
-    fprintf(f, "Vendor : %s\n\n", info->info1.vendor);
-    fprintf(f, "Stepping : 0x%x\n", info->info2.stepping);
-    fprintf(f, "Model : 0x%x\n", info->info2.model);
-    fprintf(f, "Family : 0x%x\n", info->info2.family);
-    fprintf(f, "Processor type : %s\n", info->info2.cputype);
-    fprintf(f, "Extended model : 0x%x\n", info->info2.extendedmodel);
-    fprintf(f, "Extended family : 0x%x\n\n", info->info2.extendedfamily);
+    fprintf(f, "Vendor                                     : %s\n\n", info->info1.vendor);
+    fprintf(f, "Stepping                                   : 0x%x\n", info->info2.stepping);
+    fprintf(f, "Model                                      : 0x%x\n", info->info2.model);
+    fprintf(f, "Family                                     : 0x%x\n", info->info2.family);
+    fprintf(f, "Processor type                             : %s\n", info->info2.cputype);
+    fprintf(f, "Extended model                             : 0x%x\n", info->info2.extendedmodel);
+    fprintf(f, "Extended family                            : 0x%x\n\n", info->info2.extendedfamily);
 
-    fprintf(f, "Brand ID : 0x%x\n", info->info2.brand_id);
-    fprintf(f, "Chunks : 0x%x\n", info->info2.chunks);
+    fprintf(f, "Brand ID                : 0x%x\n", info->info2.brand_id);
+    fprintf(f, "Chunks                  : 0x%x\n", info->info2.chunks);
     fprintf(f, "Logical Processor Count : 0x%x (%d)\n", info->info2.count, info->info2.count);
-    fprintf(f, "APIC ID : 0x%x\n\n", info->info2.apic_id);
+    fprintf(f, "APIC ID                 : 0x%x\n\n", info->info2.apic_id);
 
-    fprintf(f, "SSE3 : %s\n", info->info2.sse3 ? "true" : "false");
-    fprintf(f, "PCLMULDQ : %s\n", info->info2.pclmuldq ? "true" : "false");
-    fprintf(f, "DTES64 : %s\n", info->info2.dtes64 ? "true" : "false");
-    fprintf(f, "MONITOR : %s\n", info->info2.monitor ? "true" : "false");
-    fprintf(f, "DS-CPL : %s\n", info->info2.ds_cpl ? "true" : "false");
-    fprintf(f, "VMX : %s\n", info->info2.vmx ? "true" : "false");
-    fprintf(f, "SMX : %s\n", info->info2.smx ? "true" : "false");
-    fprintf(f, "EIST : %s\n", info->info2.eist ? "true" : "false");
-    fprintf(f, "TM2 : %s\n", info->info2.tm2 ? "true" : "false");
-    fprintf(f, "SSSE3 : %s\n", info->info2.ssse3 ? "true" : "false");
-    fprintf(f, "CNXT-ID : %s\n", info->info2.cnxt_id ? "true" : "false");
-    fprintf(f, "FMA : %s\n", info->info2.fma ? "true" : "false");
-    fprintf(f, "CX16 : %s\n", info->info2.cx16 ? "true" : "false");
-    fprintf(f, "XTPR : %s\n", info->info2.xtpr ? "true" : "false");
-    fprintf(f, "PDCM : %s\n", info->info2.pdcm ? "true" : "false");
-    fprintf(f, "PCID : %s\n", info->info2.pcid ? "true" : "false");
-    fprintf(f, "DCA : %s\n", info->info2.dca ? "true" : "false");
-    fprintf(f, "SSE4.1 : %s\n", info->info2.sse41 ? "true" : "false");
-    fprintf(f, "SSE4.2 : %s\n", info->info2.sse42 ? "true" : "false");
-    fprintf(f, "x2APIC : %s\n", info->info2.x2apic ? "true" : "false");
-    fprintf(f, "MOVBE : %s\n", info->info2.movbe ? "true" : "false");
-    fprintf(f, "POPCNT : %s\n", info->info2.popcnt ? "true" : "false");
+    fprintf(f, "SSE3         : %s\n", info->info2.sse3 ? "true" : "false");
+    fprintf(f, "PCLMULDQ     : %s\n", info->info2.pclmuldq ? "true" : "false");
+    fprintf(f, "DTES64       : %s\n", info->info2.dtes64 ? "true" : "false");
+    fprintf(f, "MONITOR      : %s\n", info->info2.monitor ? "true" : "false");
+    fprintf(f, "DS-CPL       : %s\n", info->info2.ds_cpl ? "true" : "false");
+    fprintf(f, "VMX          : %s\n", info->info2.vmx ? "true" : "false");
+    fprintf(f, "SMX          : %s\n", info->info2.smx ? "true" : "false");
+    fprintf(f, "EIST         : %s\n", info->info2.eist ? "true" : "false");
+    fprintf(f, "TM2          : %s\n", info->info2.tm2 ? "true" : "false");
+    fprintf(f, "SSSE3        : %s\n", info->info2.ssse3 ? "true" : "false");
+    fprintf(f, "CNXT-ID      : %s\n", info->info2.cnxt_id ? "true" : "false");
+    fprintf(f, "FMA          : %s\n", info->info2.fma ? "true" : "false");
+    fprintf(f, "CX16         : %s\n", info->info2.cx16 ? "true" : "false");
+    fprintf(f, "XTPR         : %s\n", info->info2.xtpr ? "true" : "false");
+    fprintf(f, "PDCM         : %s\n", info->info2.pdcm ? "true" : "false");
+    fprintf(f, "PCID         : %s\n", info->info2.pcid ? "true" : "false");
+    fprintf(f, "DCA          : %s\n", info->info2.dca ? "true" : "false");
+    fprintf(f, "SSE4.1       : %s\n", info->info2.sse41 ? "true" : "false");
+    fprintf(f, "SSE4.2       : %s\n", info->info2.sse42 ? "true" : "false");
+    fprintf(f, "x2APIC       : %s\n", info->info2.x2apic ? "true" : "false");
+    fprintf(f, "MOVBE        : %s\n", info->info2.movbe ? "true" : "false");
+    fprintf(f, "POPCNT       : %s\n", info->info2.popcnt ? "true" : "false");
     fprintf(f, "TSC-DEADLINE : %s\n", info->info2.tsc_deadline ? "true" : "false");
-    fprintf(f, "AES : %s\n", info->info2.aes ? "true" : "false");
-    fprintf(f, "XSAVE : %s\n", info->info2.xsave ? "true" : "false");
-    fprintf(f, "OSXSAVE : %s\n", info->info2.osxsave ? "true" : "false");
-    fprintf(f, "AVX : %s\n\n", info->info2.avx ? "true" : "false");
+    fprintf(f, "AES          : %s\n", info->info2.aes ? "true" : "false");
+    fprintf(f, "XSAVE        : %s\n", info->info2.xsave ? "true" : "false");
+    fprintf(f, "OSXSAVE      : %s\n", info->info2.osxsave ? "true" : "false");
+    fprintf(f, "AVX          : %s\n\n", info->info2.avx ? "true" : "false");
 
-    fprintf(f, "FPU : %s\n", info->info2.fpu ? "true" : "false");
-    fprintf(f, "VME : %s\n", info->info2.vme ? "true" : "false");
-    fprintf(f, "DE : %s\n", info->info2.de ? "true" : "false");
-    fprintf(f, "PSE : %s\n", info->info2.pse ? "true" : "false");
-    fprintf(f, "TSC : %s\n", info->info2.tsc ? "true" : "false");
-    fprintf(f, "MSR : %s\n", info->info2.msr ? "true" : "false");
-    fprintf(f, "PAE : %s\n", info->info2.pae ? "true" : "false");
-    fprintf(f, "MCE : %s\n", info->info2.mce ? "true" : "false");
-    fprintf(f, "CX8 : %s\n", info->info2.cx8 ? "true" : "false");
-    fprintf(f, "APIC : %s\n", info->info2.apic ? "true" : "false");
-    fprintf(f, "SEP : %s\n", info->info2.sep ? "true" : "false");
-    fprintf(f, "MTRR : %s\n", info->info2.mtrr ? "true" : "false");
-    fprintf(f, "PGE : %s\n", info->info2.pge ? "true" : "false");
-    fprintf(f, "MCA : %s\n", info->info2.mca ? "true" : "false");
-    fprintf(f, "CMOV : %s\n", info->info2.cmov ? "true" : "false");
-    fprintf(f, "PAT : %s\n", info->info2.pat ? "true" : "false");
-    fprintf(f, "PSE-36 : %s\n", info->info2.pse_36 ? "true" : "false");
-    fprintf(f, "PSN : %s\n", info->info2.psn ? "true" : "false");
-    fprintf(f, "CLFSH : %s\n", info->info2.clfsh ? "true" : "false");
-    fprintf(f, "DS : %s\n", info->info2.ds ? "true" : "false");
-    fprintf(f, "ACPI : %s\n", info->info2.acpi ? "true" : "false");
-    fprintf(f, "MMX : %s\n", info->info2.mmx ? "true" : "false");
-    fprintf(f, "FXSR : %s\n", info->info2.fxsr ? "true" : "false");
-    fprintf(f, "SSE : %s\n", info->info2.sse ? "true" : "false");
-    fprintf(f, "SSE2 : %s\n", info->info2.sse2 ? "true" : "false");
-    fprintf(f, "SS : %s\n", info->info2.ss ? "true" : "false");
-    fprintf(f, "HTT : %s\n", info->info2.htt ? "true" : "false");
-    fprintf(f, "TM : %s\n", info->info2.tm ? "true" : "false");
-    fprintf(f, "PBE : %s\n", info->info2.pbe ? "true" : "false");
+    fprintf(f, "FPU          : %s\n", info->info2.fpu ? "true" : "false");
+    fprintf(f, "VME          : %s\n", info->info2.vme ? "true" : "false");
+    fprintf(f, "DE           : %s\n", info->info2.de ? "true" : "false");
+    fprintf(f, "PSE          : %s\n", info->info2.pse ? "true" : "false");
+    fprintf(f, "TSC          : %s\n", info->info2.tsc ? "true" : "false");
+    fprintf(f, "MSR          : %s\n", info->info2.msr ? "true" : "false");
+    fprintf(f, "PAE          : %s\n", info->info2.pae ? "true" : "false");
+    fprintf(f, "MCE          : %s\n", info->info2.mce ? "true" : "false");
+    fprintf(f, "CX8          : %s\n", info->info2.cx8 ? "true" : "false");
+    fprintf(f, "APIC         : %s\n", info->info2.apic ? "true" : "false");
+    fprintf(f, "SEP          : %s\n", info->info2.sep ? "true" : "false");
+    fprintf(f, "MTRR         : %s\n", info->info2.mtrr ? "true" : "false");
+    fprintf(f, "PGE          : %s\n", info->info2.pge ? "true" : "false");
+    fprintf(f, "MCA          : %s\n", info->info2.mca ? "true" : "false");
+    fprintf(f, "CMOV         : %s\n", info->info2.cmov ? "true" : "false");
+    fprintf(f, "PAT          : %s\n", info->info2.pat ? "true" : "false");
+    fprintf(f, "PSE-36       : %s\n", info->info2.pse_36 ? "true" : "false");
+    fprintf(f, "PSN          : %s\n", info->info2.psn ? "true" : "false");
+    fprintf(f, "CLFSH        : %s\n", info->info2.clfsh ? "true" : "false");
+    fprintf(f, "DS           : %s\n", info->info2.ds ? "true" : "false");
+    fprintf(f, "ACPI         : %s\n", info->info2.acpi ? "true" : "false");
+    fprintf(f, "MMX          : %s\n", info->info2.mmx ? "true" : "false");
+    fprintf(f, "FXSR         : %s\n", info->info2.fxsr ? "true" : "false");
+    fprintf(f, "SSE          : %s\n", info->info2.sse ? "true" : "false");
+    fprintf(f, "SSE2         : %s\n", info->info2.sse2 ? "true" : "false");
+    fprintf(f, "SS           : %s\n", info->info2.ss ? "true" : "false");
+    fprintf(f, "HTT          : %s\n", info->info2.htt ? "true" : "false");
+    fprintf(f, "TM           : %s\n", info->info2.tm ? "true" : "false");
+    fprintf(f, "PBE          : %s\n", info->info2.pbe ? "true" : "false");
 
     fprintf(f, "\nCaches and TLB descriptor :\n");
     for(i=0; i < 15; ++i)
