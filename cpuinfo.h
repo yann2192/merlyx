8a7b7b7ff5f7150a64153acb9065216aed0cd914
Yann GUIBET <yannguibet@gmail.com>
2012-03-17 12:46:48 +0100

    differenciation de intel et amd, amelioration de l'implementation

--- cpuinfo.h
+++ cpuinfo.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "intel_cpuinfo.h"
+#include "amd_cpuinfo.h"
 
 struct eax0
 {
@@ -13,129 +15,26 @@ struct eax0
 
 typedef struct eax0 EAX0;
 
-struct eax1
-{
-    unsigned char stepping;
-    unsigned char model;
-    unsigned char family;
-    char * cputype;
-    unsigned char extendedmodel;
-    unsigned short extendedfamily;
-
-    unsigned char brand_id;
-    unsigned char chunks;
-    unsigned char count;
-    unsigned char apic_id;
-
-    unsigned char sse3;
-    unsigned char pclmuldq;
-    unsigned char dtes64;
-    unsigned char monitor;
-    unsigned char ds_cpl;
-    unsigned char vmx;
-    unsigned char smx;
-    unsigned char eist;
-    unsigned char tm2;
-    unsigned char ssse3;
-    unsigned char cnxt_id;
-    unsigned char fma;
-    unsigned char cx16;
-    unsigned char xtpr;
-    unsigned char pdcm;
-    unsigned char pcid;
-    unsigned char dca;
-    unsigned char sse41;
-    unsigned char sse42;
-    unsigned char x2apic;
-    unsigned char movbe;
-    unsigned char popcnt;
-    unsigned char tsc_deadline;
-    unsigned char aes;
-    unsigned char xsave;
-    unsigned char osxsave;
-    unsigned char avx;
-
-    unsigned char fpu;
-    unsigned char vme;
-    unsigned char de;
-    unsigned char pse;
-    unsigned char tsc;
-    unsigned char msr;
-    unsigned char pae;
-    unsigned char mce;
-    unsigned char cx8;
-    unsigned char apic;
-    unsigned char sep;
-    unsigned char mtrr;
-    unsigned char pge;
-    unsigned char mca;
-    unsigned char cmov;
-    unsigned char pat;
-    unsigned char pse_36;
-    unsigned char psn;
-    unsigned char clfsh;
-    unsigned char ds;
-    unsigned char acpi;
-    unsigned char mmx;
-    unsigned char fxsr;
-    unsigned char sse;
-    unsigned char sse2;
-    unsigned char ss;
-    unsigned char htt;
-    unsigned char tm;
-    unsigned char pbe;
-};
-
-typedef struct eax1 EAX1;
-
-struct eax2
-{
-    char * caches[15]; /* NOTE: maybe change */
-};
-typedef struct eax2 EAX2;
-
-struct eax4
-{
-    char * cache_type;
-    unsigned char cache_level;
-    unsigned char sicl;
-    unsigned char fac;
-    unsigned short mntstc;
-    unsigned char napicidrtp;
-
-    unsigned short scls;
-    unsigned short plp;
-    unsigned short woa;
-
-    unsigned int nsets;
-
-    unsigned char wbinvd;
-    unsigned char ciitlcl;
-    unsigned char cci;
-
-    unsigned short cache_size;
-
-    struct eax4 * next;
-};
-typedef struct eax4 EAX4;
-
 struct cpuid_info
 {
     EAX0 info1;
-    EAX1 info2;
-    EAX2 info3;
-    EAX4 info4;
+    union {
+        intel_EAX1 intel;
+        amd_EAX1 amd;
+    } info2;
+    union {
+        intel_EAX2 intel;
+        amd_EAX2 amd;
+    } info3;
+    union {
+        intel_EAX4 intel;
+        amd_EAX4 amd;
+    } info4;
 };
 
 typedef struct cpuid_info * CPUID_INFO;
 
 void getVendor(EAX0 *);
-
-void getProcessorInfo(EAX1 *);
-
-void getCachesInfo(EAX2 *);
-
-void getCachesParameters(EAX4 *);
 
 CPUID_INFO CPUID_INFO_create();
 
